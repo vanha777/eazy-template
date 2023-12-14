031b061a4a26975eee2908f67d7b9ed79effd2d4
CopyCoder(VJ) <107760796+vanha777@users.noreply.github.com>
2023-12-15 09:16:18 +1100

    test .git workflow

---
+++ services/service_eazypic/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# setup.sh
+
+# Ensure non-interactive installation
+export DEBIAN_FRONTEND=noninteractive
+
+# Update package list
+apt-get update
+
+# Install wkhtmltopdf
+apt-get install -y wkhtmltopdf
+
+# Install pkg-config
+apt-get install -y pkg-config
+
+# Install OpenSSL development libraries
+apt-get install -y libssl-dev
+
+# Install Apache HTTP Server (equivalent to httpd in Yum)
+apt-get install -y apache2
+
+# Install OpenSSL (the package name might just be 'openssl' in Debian/Ubuntu)
+apt-get install -y openssl
+
+# Clean up the package cache to reduce image size
+apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Compile the Rust application
+cargo build --release --target x86_64-unknown-linux-musl
