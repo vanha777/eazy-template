7882fb2af5c715e53b64352f675b79da613679ad
CopyCoder(VJ) <107760796+vanha777@users.noreply.github.com>
2023-12-16 08:15:53 +1100

    5

--- services/service_docurust/setup.sh
+++ services/service_docurust/setup.sh
@@ -10,20 +10,20 @@ apt-get update
 # Install wkhtmltopdf
 apt-get install -y wkhtmltopdf
 
-# Install pkg-config
-apt-get install -y pkg-config
+# Install OpenSSL (the package name might just be 'openssl' in Debian/Ubuntu)
+apt-get install -y openssl
 
 # Install OpenSSL development libraries
 apt-get install -y libssl-dev
 
+# Install pkg-config
+apt-get install -y pkg-config
+
 # Install Apache HTTP Server (equivalent to httpd in Yum)
 apt-get install -y apache2
-
-# Install OpenSSL (the package name might just be 'openssl' in Debian/Ubuntu)
-apt-get install -y openssl
 
 # Clean up the package cache to reduce image size
 apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Compile the specific Rust service
-cargo build --release --target x86_64-unknown-linux-musl -p service_docurust
+cargo build --release -p service_docurust
