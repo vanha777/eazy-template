1538cb9c8d1cf288ab222a4cbf04daf32caca0c0
CopyCoder(VJ) <107760796+vanha777@users.noreply.github.com>
2023-12-16 17:52:51 +1100

    latest build

--- services/service_eazypic/setup.sh
+++ services/service_eazypic/setup.sh
@@ -7,23 +7,23 @@ export DEBIAN_FRONTEND=noninteractive
 # Update package list
 apt-get update
 
-# # Install wkhtmltopdf
-# apt-get install -y wkhtmltopdf
-
-# Install OpenSSL (the package name might just be 'openssl' in Debian/Ubuntu)
-apt-get install -y openssl
-
-# Install OpenSSL development libraries
-apt-get install -y libssl-dev
+# Install wkhtmltopdf
+apt-get install -y wkhtmltopdf
 
 # Install pkg-config
 apt-get install -y pkg-config
 
+# Install OpenSSL development libraries
+apt-get install -y libssl-dev
+
 # Install Apache HTTP Server (equivalent to httpd in Yum)
 apt-get install -y apache2
+
+# Install CA certificates
+apt-get install -y ca-certificates
 
 # Clean up the package cache to reduce image size
 apt-get clean && rm -rf /var/lib/apt/lists/*
 
-# Compile the specific Rust service
-cargo build --release -p service_eazypic
+# Compile the Rust application
+cargo build --release --target x86_64-unknown-linux-musl -p service_eazypic
